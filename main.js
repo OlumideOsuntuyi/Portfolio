7bbccbd00a030ff4b97ce5752d7eb39d75cff73f
olumide osuntuyi <olugeorge61@gmail.com>
2024-09-05 22:12:14 +0100

    Added Text Fade In and Out

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-<script>
+document.addEventListener("DOMContentLoaded", function() {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("is-visible");
+      } else {
+        entry.target.classList.remove("is-visible");
+      }
+    });
+  });
 
-</script>
+  document.querySelectorAll('#personal-projects .grid-container .grid-section .info-body').forEach(section => {
+    observer.observe(section);
+  });
+});
