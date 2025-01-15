a5605a154348fe30733ce735837b6006b70be129
olumide osuntuyi <olugeorge61@gmail.com>
2025-01-15 01:07:26 +0100

    Added switchable personal projects

--- main.js
+++ main.js
@@ -19,3 +19,48 @@ document.addEventListener("DOMContentLoaded", function() {
 
 });
 
+var plCounters = null;
+var plElementsArray = null;
+var plIndex = 0;
+var plInterval = 1000 * 2;
+ 
+function toggleClass() 
+{ 
+  // Get all elements with the class 'pl-a' that share the same parent
+  // Add 'pl-h' to the current element while removing from others
+  // Loop index 
+  var parent = plElementsArray[0].parentNode; 
+  var siblings = Array.from(parent.querySelectorAll('.pl-a'));
+  siblings.forEach(function(element) 
+  { 
+    if(!element.classList.contains('pl-h'))
+    {
+      element.classList.add('pl-h'); 
+    }
+  }); 
+
+  siblings[plIndex].classList.remove('pl-h'); 
+  setPLCounters();
+  plIndex = (plIndex + 1) % siblings.length;
+}
+
+function setPLCounters()
+{
+  plCounters.forEach(function(element)
+  {
+    element.textContent = `Project ${plIndex + 1} of ${plElementsArray.length}`;
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function() { 
+  // stores all elements of class pl-a
+  var elements = document.querySelectorAll('.pl-a'); 
+  plElementsArray = Array.from(elements);
+
+  
+  var elements2 = document.querySelectorAll('.pl-counter'); 
+  plCounters = Array.from(elements2);
+  toggleClass();
+});
+
+
